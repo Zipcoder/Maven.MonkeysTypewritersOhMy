58ef1f9b0aad9649cb1e2e0f05f5656ae2eea2e6
wes <wes.jones1597@gmail.com>
2019-12-12 09:45:21 -0500

    corrupted and noncorrupted threads doin' their thang

--- src/main/java/io/zipcoder/MonkeyTypewriter.java
+++ src/main/java/io/zipcoder/MonkeyTypewriter.java
@@ -24,6 +24,32 @@ public class MonkeyTypewriter {
         // For each Copier(one safe and one unsafe), create and start 5 monkeys copying the introduction to
         // A Tale Of Two Cities.
 
+        // Unsafe Copier
+        Thread[] unsafeThreads = new Thread[5];
+        UnsafeCopier unsafeCopier = new UnsafeCopier(introduction);
+        for (int i = 0; i < 5; i++) {
+            unsafeThreads[i] = new Thread(unsafeCopier);
+        }
+        for (int i = 0; i < unsafeThreads.length; i++) {
+            unsafeThreads[i].start();
+        }
+
+        // Safe Copier
+        Thread[] safeThreads = new Thread[5];
+        SafeCopier safeCopier = new SafeCopier(introduction);
+        try {
+            for (int i = 0; i < 5; i++) {
+                safeThreads[i] = new Thread(safeCopier);
+            }
+            for (Thread thread : safeThreads) {
+                thread.start();
+            }
+            for (Thread thread : safeThreads) {
+                thread.join();
+            }
+        } catch(InterruptedException e) {
+            e.printStackTrace();
+        }
 
         // This wait is here because main is still a thread and we want the main method to print the finished copies
         // after enough time has passed.
@@ -34,5 +60,8 @@ public class MonkeyTypewriter {
         }
 
         // Print out the copied versions here.
+
+        System.out.println("[UNSAFE]: " + unsafeCopier.copied);
+        System.out.println("[SAFE]:   " + safeCopier.copied);
     }
 }
--- src/main/java/io/zipcoder/SafeCopier.java
+++ src/main/java/io/zipcoder/SafeCopier.java
@@ -1,8 +1,36 @@
 package io.zipcoder;
 
+import java.util.Random;
+
 /**
  * Make this extend the Copier like `UnsafeCopier`, except use locks to make sure that the actual intro gets printed
  * correctly every time.  Make the run method thread safe.
  */
-public class SafeCopier {
+public class SafeCopier extends Copier {
+    public SafeCopier(String toCopy) {
+        super(toCopy);
+    }
+
+    public void run() {
+        String nextWord = "";
+        try {
+            while(this.stringIterator.hasNext()) {
+//                nextWord = this.stringIterator.next() + " ";
+//                Thread.sleep(new Random().nextInt(50));
+//                this.copied += nextWord;
+                getWord();
+            }
+        } catch(InterruptedException ie) {
+            ie.printStackTrace();
+        }
+    }
+
+    private synchronized void getWord() throws InterruptedException {
+        if (this.stringIterator.hasNext()) {
+            String nextWord = this.stringIterator.next() + " ";
+            Thread.sleep(new Random().nextInt(50));
+            System.out.println(Thread.currentThread().getName());
+            this.copied += nextWord;
+        }
+    }
 }
