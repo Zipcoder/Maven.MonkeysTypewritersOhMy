08a66842aad52e96ae94cc8325650b1621f4fd19
vaidehi4321 <56602405+vaidehi4321@users.noreply.github.com>
2020-01-16 09:00:42 -0500

    completed

--- src/main/java/io/zipcoder/SafeCopier.java
+++ src/main/java/io/zipcoder/SafeCopier.java
@@ -1,8 +1,35 @@
 package io.zipcoder;
 
+import java.util.Random;
+
 /**
  * Make this extend the Copier like `UnsafeCopier`, except use locks to make sure that the actual intro gets printed
  * correctly every time.  Make the run method thread safe.
  */
-public class SafeCopier {
+public class SafeCopier extends Copier {
+    public SafeCopier(String toCopy) {
+        super(toCopy);
+    }
+
+    public void run() {
+        Random rm = new Random();
+        int rmSleep = rm.nextInt((150-50) + 1)+50;
+        while(stringIterator.hasNext()){
+            try {
+                {
+                    synchronized (stringIterator) {
+                        String holder = stringIterator.next() + " ";
+                        Thread.sleep(rmSleep);
+                        System.out.println(Thread.currentThread().getName() + " " + holder);
+                        copied = copied + holder;
+                    }
+                }
+            }
+            catch (InterruptedException ex){
+                ex.printStackTrace();
+            }
+
+        }
+        copied = copied.trim();
+    }
 }
